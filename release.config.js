5b70df89e5974410ae41b8663bff629b3f8e801c
wirekang <wirekang@gmail.com>
2023-03-13 16:36:52 +0900

    build: update release config

--- release.config.js
+++ release.config.js
@@ -2,7 +2,7 @@ const config = {
     repositoryUrl: "https://github.com/wirekang/mouseable3",
     branches: ["main"],
     plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', ['@semantic-release/github', {
-        assets: ["dist/"]
+        assets: ["dist/*"]
     }]]
 }
 
