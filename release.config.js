9398ee05a3bf52b6096313ef44873eca530bb311
wirekang <wirekang@gmail.com>
2023-03-13 16:09:34 +0900

    build: remove dev branch from ci

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 const config = {
     repositoryUrl:"https://github.com/wirekang/mouseable3",
-    branches: ["main", "beta"],
+    branches: ["main"],
     plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', ['@semantic-release/github', {
         assets: "dist"
     }]]
