05973cbc91e3280b11d7896f217037ca49ae6b33
wirekang <wirekang@gmail.com>
2023-03-13 15:26:14 +0900

    build: move repositoryUrl to package.json

--- release.config.js
+++ release.config.js
@@ -1,5 +1,4 @@
 const config = {
-    repositoryUrl: "https://github.com/wirekang/mouseable3.git",
     branches: ["main", "beta"],
     plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', ['@semantic-release/github', {
         assets: "dist"
