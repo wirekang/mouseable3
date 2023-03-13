078f4a5e75242255fdffd2326f897fa1387fd06a
wirekang <wirekang@gmail.com>
2023-03-13 15:38:15 +0900

    build: set permissions

--- release.config.js
+++ release.config.js
@@ -1,4 +1,5 @@
 const config = {
+    repositoryUrl:"https://github.com/wirekang/mouseable3",
     branches: ["main", "beta"],
     plugins: ['@semantic-release/commit-analyzer', '@semantic-release/release-notes-generator', ['@semantic-release/github', {
         assets: "dist"
