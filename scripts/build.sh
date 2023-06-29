39e088a71792a08ed828238ca47fb7396f5452f0
wirekang <wirekang@gmail.com>
2023-06-29 17:58:05 +0900

    init cpp

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,9 @@
 
 rm -rf dist
 mkdir -p dist || exit 1
+cd dist || exit 1
 
-echo "$1"
-echo "$2"
+export VERSION="$1"
+export BRANCH="$2"
+cmake .. || exit 1
+make || exit 1
---
+++ src/main.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <X11/Xlib.h>
+#include <unistd.h>
+
+int main() {
+    std::cout << "Hello" << std::endl;
+
+    Display *d = XOpenDisplay(nullptr);
+    if (d) {
+        Window w = XCreateWindow(d, DefaultRootWindow(d), 0, 0, 200, 100, 0, CopyFromParent, CopyFromParent,
+                                 CopyFromParent, 0, nullptr);
+        XMapWindow(d, w);
+        XFlush(d);
+        sleep(2);
+    }
+    return 0;
+}
